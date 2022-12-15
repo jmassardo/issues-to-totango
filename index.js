c9866158b660f8696ebe74d3a8f2d3cc29c4a5f9
Collin McNeese <collinmcneese@github.com>
2022-12-15 16:25:53 -0600

    eslint updates
    Signed-off-by: Collin McNeese <collinmcneese@github.com>
    Signed-off-by: rockin-robinson <rockin-robinson@github.com>

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 
 try {
   // Fetch variables from the actions inputs
-  const TOTANGO_API_URL = "https://api.totango.com/api/v3/touchpoints/";
+  const TOTANGO_API_URL = 'https://api.totango.com/api/v3/touchpoints/';
   const ACCOUNT_ID = core.getInput('ACCOUNT_ID');
   const APP_TOKEN = core.getInput('APP_TOKEN');
   const ACTIVITY_TYPE = core.getInput('ACTIVITY_TYPE');
@@ -12,7 +12,7 @@ try {
 
   // Fetch the payload from the event
   const issue = github.context.payload.issue;
-  console.log(`Issue num is: ${issue["number"]}`);
+  console.log(`Issue num is: ${issue['number']}`);
 
   const comment = github.context.payload.comment;
   console.log(`eventName is: ${github.context.eventName}`);
@@ -20,39 +20,39 @@ try {
   const event_action = github.context.payload.action;
   console.log(`Event Action is: ${event_action}`);
 
-  //Build payload body
+  // Build payload body
 
-  if (github.context.eventName == "issues") {
+  if (github.context.eventName === 'issues') {
 
-    if (event_action == "opened") {
+    if (event_action === 'opened') {
 
-      var subject = "New Issue: " + issue["title"];
-      var body = `${issue["user"]["login"]} created a  new issue. ${issue["body"]}. More info here: ${issue["html_url"]}`
+      var subject = 'New Issue: ' + issue['title'];
+      var body = `${issue['user']['login']} created a  new issue. ${issue['body']}. More info here: ${issue['html_url']}`;
 
-    } else if (event_action == "closed") {
+    } else if (event_action === 'closed') {
 
-      var subject = "Issue #: " + issue["title"] + " was closed";
-      var body = `${issue["user"]["login"]} closed an issue. ${issue["body"]}. More info here: ${issue["html_url"]}`
+      var subject = 'Issue #: ' + issue['title'] + ' was closed';
+      var body = `${issue['user']['login']} closed an issue. ${issue['body']}. More info here: ${issue['html_url']}`;
 
     }
 
-  } else if (github.context.eventName == "issue_comment") {
+  } else if (github.context.eventName === 'issue_comment') {
 
-    var subject = "New comment on issue: " + issue["number"];
-    var body = `${comment["user"]["login"]} commented on issue #${issue["number"]}. ${comment["body"]}. More info here: ${issue["html_url"]}`
-  
+    var subject = 'New comment on issue: ' + issue['number'];
+    var body = `${comment['user']['login']} commented on issue #${issue['number']}. ${comment['body']}. More info here: ${issue['html_url']}`;
+
   } else {
 
-    core.setFailed("Unsupported event type. Please use the  `issues` or `issue_comment` event type.");
+    core.setFailed('Unsupported event type. Please use the  `issues` or `issue_comment` event type.');
 
   }
-  
+
   // output the payload to the console so the user can see it
   console.log(`Touchpoint subject is: ${subject}`);
   console.log(`Touchpoint body is: ${body}`);
 
   // Build the POST Request
-  var request = require("request"); 
+  var request = require('request');
 
   request.post(TOTANGO_API_URL, {
     headers: {
@@ -65,12 +65,12 @@ try {
       subject: subject,
       touchpointType: TOUCHPOINT_TYPE,
       touchpoint_tags: [ TOUCHPOINT_TAGS ],
-    }
+    },
   }, (error, response, body) => {
     // Output a message to the console and an Action output
-    touchpoint_id = (JSON.parse(response.body))["note"]["id"]
+    touchpoint_id = (JSON.parse(response.body))['note']['id'];
     console.log(`Successfully created touchpoint: ${touchpoint_id}`);
-    core.setOutput("touchpoint_id", touchpoint_id);
+    core.setOutput('touchpoint_id', touchpoint_id);
     console.log(response.statusCode);
   });
 
