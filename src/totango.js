174d242524e1b56a27c61c8570846fbdf920ff89
Cheerag Patel <cheeragpatel@github.com>
2023-02-24 13:55:15 -0500

    Set issue to the correct type
    Signed-off-by: Cheerag Patel <cheeragpatel@github.com>

--- src/totango.js
+++ src/totango.js
@@ -292,7 +292,7 @@ async function labeled({ issue, label }) {
   let subject = 'Issue #: ' + issue['title'] + ' was labeled';
   let body = `${issue['user']['login']} labeled an issue. ${issue['body']}. More info here: ${issue['html_url']}`;
   // check if issue already has a task or touchpoint associated with it
-  if (await issue_has_task_id(issue), 'task') {
+  if (await issue_has_task_id({issue: issue}, 'task')) {
       console.log('Issue already has task id');
       return;
   }
